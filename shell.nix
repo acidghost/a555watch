b344d7a680950907ed2edbb036241f0f53be06b0
acidghost <1787979+acidghost@users.noreply.github.com>
2025-02-15 20:54:23 +0100

    chore: update nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  # nixpkgs-unstable as of 2025-02-15 at 19:14
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/1128e89fd5e11bb25aedbfc287733c6502202ea9.tar.gz";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
